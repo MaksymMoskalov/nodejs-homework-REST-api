fb54a6b2cafd690247188e010ecc295ee6facf6b
Maksim <max.godnavi133@gmail.com>
2024-01-19 14:50:26 +0200

    Home done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { validationBody, authenticate } = require("../../middlewares");
+const { validationBody, authenticate, upload } = require("../../middlewares");
 const { registerShema } = require("../../schemas");
 const {
   register,
   login,
   getCurrent,
   logout,
+  updateAvatar,
 } = require("../../controllers/auth");
 
 const router = express.Router();
@@ -14,4 +15,5 @@ router.post("/register", validationBody(registerShema), register);
 router.post("/login", validationBody(registerShema), login);
 router.get("/current", authenticate, getCurrent);
 router.post("/logout", authenticate, logout);
+router.patch("/avatars", authenticate, upload.single("avatar"), updateAvatar);
 module.exports = router;
