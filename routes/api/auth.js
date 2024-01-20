c56ce6f43ba710b46b241d63f03be666ccbccb87
Maksim <max.godnavi133@gmail.com>
2024-01-20 18:06:10 +0200

    home done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const { validationBody, authenticate, upload } = require("../../middlewares");
-const { registerShema } = require("../../schemas");
+const { registerShema, emailShema } = require("../../schemas");
 const {
   register,
   login,
   getCurrent,
   logout,
   updateAvatar,
+  verifyEmail,
+  resendVerifyEmail,
 } = require("../../controllers/auth");
 
 const router = express.Router();
 
 router.post("/register", validationBody(registerShema), register);
+router.get("/verify/:verificationToken", verifyEmail);
+router.post("/verify", validationBody(emailShema), resendVerifyEmail);
 router.post("/login", validationBody(registerShema), login);
 router.get("/current", authenticate, getCurrent);
 router.post("/logout", authenticate, logout);
--- service/index.js
+++ service/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpErrors");
 const ctrlWrapper = require("./contrillerWraper");
 const MongooseError = require("./MongoosError");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   MongooseError,
+  sendEmail,
 };
